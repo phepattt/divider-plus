6c6e3846aaee4abbbc068801775a71d8ece76b94
PEERAPAT SATTAPORNNARA <63010702@KMITL.AC.TH>
2020-08-30 22:34:17 +0700

    Add project files.

---
+++ divider plus/ผมงงครับจาร2.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+main()
+{
+	int a, b = 1, c = 0;
+	printf("Enter number : ");
+	scanf("%d", &a);
+	while (b < a)
+	{
+		if (a % b == 0)
+		{
+			c = c + b;
+			b = b + 1;
+		}
+		else
+		{
+			b = b + 1;
+		}
+	}
+	if (a = b)
+	{
+		printf("%d", c);
+	}
+	return 0;
+}
